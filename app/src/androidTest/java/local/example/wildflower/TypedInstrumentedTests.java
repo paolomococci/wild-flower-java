cc5a798a34ea2f12b0a03676cfca12624ccba0df
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-03-24 22:06:18 +0100

    Create TypedInstrumentedTests.java

---
+++ app/src/androidTest/java/local/example/wildflower/TypedInstrumentedTests.java
@@ -0,0 +1,39 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.wildflower;
+
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class TypedInstrumentedTests {
+
+    @Test
+    public void useAppContext() {
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        assertEquals("local.example.sunflower", appContext.getPackageName());
+    }
+}
